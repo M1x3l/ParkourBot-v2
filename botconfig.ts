27740a91994b17a8e1945d7acbe300819fc4401f
M1x3l <michael.aus.oberkail@outlook.de>
2021-09-06 21:51:22 +0200

    added check for valid channel and permissions

--- botconfig.ts
+++ botconfig.ts
@@ -1,41 +1,44 @@
+import { PermissionResolvable, PremiumTier, Snowflake } from 'discord.js';
+
 // Servers the bot should operate in
-const servers = new Array(
+export const servers: Snowflake[] = [
 	'774265785455083531', // Bot testing server
 	'796365707976900649', // Mixel's other test server
-	'746681304111906867' // Steve's Underwater Paradise
-);
+	'746681304111906867', // Steve's Underwater Paradise
+];
 
 // Embed colors that should be used
-const embedColors = new Array(
+export const embedColors = [
 	'#0099FF', // Info
-	'#FF0000' // Error
-);
+	'#FF0000', // Error
+];
 
 // Maps booleans to emojis
-const boolToEmojiMap = new Map<boolean, string>([
+export const boolToEmojiMap = new Map<boolean, string>([
 	[false, ':x:'],
 	[true, ':white_check_mark:'],
 ]);
 
 // The member count voice channels that should be updated
-const memberCountVoiceChannelIDs = new Array(
+export const memberCountVoiceChannelIDs: Snowflake[] = [
 	'818456788113948743', // Steve's Underwater Paradise
-	'815221664320192522' // Bot testing server
-);
+	'815221664320192522', // Bot testing server
+];
 
 // Maps server boost levels to strings
-const serverBoostLevelMap = new Map<string, string>([
+export const serverBoostLevelMap = new Map<PremiumTier, string>([
 	['NONE', 'no level'], // No server boosts
 	['TIER_1', 'level 1'], // Boost level 1
 	['TIER_2', 'level 2'], // Boost level 2
 	['TIER_3', 'level 3'], // Boost level 3
-	['TIER_4', 'level 4'], // Boost level 4
 ]);
 
-export {
-	servers,
-	embedColors,
-	boolToEmojiMap,
-	memberCountVoiceChannelIDs,
-	serverBoostLevelMap,
-};
+// Allowed command channel names
+export const allowedChatInputChannelNames = ['bots'];
+
+export const bypassCommandWhitelistPermissions: PermissionResolvable[] = [
+	'ADMINISTRATOR',
+	'MANAGE_CHANNELS',
+	'MANAGE_CHANNELS',
+	'MANAGE_GUILD',
+];
--- events/interactionCreate.ts
+++ events/interactionCreate.ts
@@ -1,8 +1,31 @@
 import { Interaction } from 'discord.js';
+import {
+	allowedChatInputChannelNames,
+	bypassCommandWhitelistPermissions,
+} from '../botconfig';
 import { chatInputCommands, userCommands } from '../Util';
 
 export async function run(interaction: Interaction) {
+	if (interaction.channel?.type == 'DM') return;
+
 	if (interaction.isCommand()) {
+		if (
+			!(
+				allowedChatInputChannelNames.includes(
+					interaction.channel?.name || ''
+				) ||
+				(
+					await interaction.guild?.members.fetch(interaction.user.id)!
+				).permissions.any(bypassCommandWhitelistPermissions)
+			)
+		) {
+			interaction.reply({
+				content:
+					'Sorry, you are not allowed to use slash commands in this channel, if there is a context-menu command with a similar behaviour, please use that one',
+				ephemeral: true,
+			});
+			return;
+		}
 		if (!chatInputCommands.has(interaction.commandName)) return;
 
 		try {
